b67c30ba0ff762031a92811468dc3a2d5f9a41f9
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-04-26 13:10:26 +0200

    Add urls and needed views

--- network/urls.py
+++ network/urls.py
@@ -4,8 +4,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="index"),  # default route
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("following", views.following, name="following"),
+    path("profile/<str:user>", views.profile, name="profile"),
+
+    # API Routes
+    path("edit/<str:post_id>", views.edit, name="edit"),
+    path("like/<str:post_id>", views.like, name="like")
 ]
--- network/views.py
+++ network/views.py
@@ -1,14 +1,39 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from django.core.paginator import Paginator
+from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+from .models import User, Post, Follow, Like
 
 
 def index(request):
-    return render(request, "network/index.html")
+    if request.method == "POST":
+        user = request.user
+        post = request.POST["post"]
+        timestamp = datetime.now()
+        if post != "":
+            Post.objects.create(user=user, post=post, timestamp=timestamp, likes=0)
+
+    posts = Post.objects.all().order_by('-timestamp')  # most recent post first
+
+    for post in posts:
+        post.likes = Like.objects.filter(post=post.id).count()
+        post.save()
+
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "network/index.html", {
+        "page_obj": page_obj  # most recent post first
+    })
 
 
 def login_view(request):
@@ -61,3 +86,87 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+
+# =============================================================================
+@login_required
+def following(request):
+    user = request.user
+    following = Follow.objects.filter(follower=request.user).values('following_id')
+
+    posts = Post.objects.filter(user__in=following).order_by('-timestamp')
+    for post in posts:
+        post.likes = Like.objects.filter(post=post.id).count()
+        post.save()
+
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "network/following.html", {
+        "page_obj": page_obj
+    })
+
+
+def profile(request, user):
+    user = User.objects.get(id=user)
+    button = "Follow" if Follow.objects.filter(follower=request.user, following=user).count() == 0 else "Unfollow"
+
+    if request.method == "POST":
+        if request.POST["button"] == "Follow":
+            button = "Unfollow"
+            Follow.objects.create(follower=request.user, following=user)
+        else:
+            button = "Follow"
+            Follow.objects.get(follower=request.user, following=user).delete()
+
+    posts = Post.objects.filter(user=user.id).order_by('-timestamp')
+    for post in posts:
+        post.likes = Like.objects.filter(post=post.id).count()
+        post.save()
+
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "network/profile.html", {
+        "user": user,
+        "followers": Follow.objects.filter(following=user).count(),
+        "following": Follow.objects.filter(follower=user).count(),
+        "page_obj": page_obj,
+        "button": button
+    })
+
+
+@csrf_exempt
+@login_required
+def edit(request, post_id):
+    post = Post.objects.get(id=post_id)
+
+    if request.method == "PUT":
+        data = json.loads(request.body)
+        if data.get("post") is not None:
+            post.post = data["post"]
+        post.save()
+        return HttpResponse(status=204)
+
+
+@csrf_exempt
+@login_required
+def like(request, post_id):
+    post = Post.objects.get(id=post_id)
+
+    if request.method == "GET":
+        return JsonResponse(post.serialize())
+
+    if request.method == "PUT":
+        data = json.loads(request.body)
+        print(data.get("like"))
+        if data.get("like"):
+            Like.objects.create(user=request.user, post=post)
+            post.likes = Like.objects.filter(post=post).count()
+        else:  # unlike
+            Like.objects.filter(user=request.user, post=post).delete()
+            post.likes = Like.objects.filter(post=post).count()
+        post.save()
+        return HttpResponse(status=204)
